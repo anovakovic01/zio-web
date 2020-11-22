c430c77242d5dc18b1f55982a133e55be90d229a
Aleksandar Novakovic <anovakovic01@gmail.com>
2020-11-22 12:10:57 +0100

    Add initial test for the logging middleware
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

---
+++ core/src/test/scala/zio/web/HttpMiddlewareSpec.scala
@@ -0,0 +1,33 @@
+import java.io.{ FileOutputStream, IOException }
+import java.net.URI
+
+import zio._
+import zio.blocking.Blocking
+import zio.clock.Clock
+import zio.test.Assertion._
+import zio.test._
+import zio.web.http
+import zio.web.http.HttpRequest
+import zio.web.http.model.{ Method, Route }
+import zio.web.schema.Schema
+
+object HttpMiddlewareSpec extends DefaultRunnableSpec with http.HttpProtocolModule {
+  import http.HttpMiddleware
+
+  // TODO: should read logs from file and assert content
+  def spec =
+    suite("HttpMiddleware")(
+      suite("logging")(
+        testM("write to file") {
+          val dest = HttpMiddleware.fileSink("test.log")
+
+          for {
+            l      <- HttpMiddleware.logging(dest).make
+            result <- l.request.processor(("GET", new URI("http://zio.dev")))
+            _      <- ZIO.effect { println(result) }
+          } yield assert(true)(equalTo(true))
+        }
+      )
+    ).provideCustomLayerShared(Blocking.live ++ Clock.live)
+
+}
